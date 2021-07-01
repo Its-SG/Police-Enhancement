31a7168daf5f93a97ec7badb6087282472dcbd7c
Sarthak Gautam <84192817+Its-SG@users.noreply.github.com>
2021-07-01 12:48:21 +0530

    Add files via upload

---
+++ PoliceEnhancement.net/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PoliceEnhancement.net")>
+<Assembly: AssemblyDescription("Police Enhancement")>
+<Assembly: AssemblyCompany("SG Mods ©  2020")>
+<Assembly: AssemblyProduct("PoliceEnhancement")>
+<Assembly: AssemblyCopyright("SG Mods Copyright ©  2020")>
+<Assembly: AssemblyTrademark("SG MODS Copyright ©  2020")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("fa2c716a-dbaf-4231-bf83-4e9980a5a54c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("10.0.0.0")>
+<Assembly: AssemblyFileVersion("10.0.0.0")>
